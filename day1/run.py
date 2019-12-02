a52d24a0d54130fa8f36b654effacb2de742fda2
Manuel Rothenberg <manu.rothenberg@gmail.com>
2019-12-02 10:39:50 +0100

    Day 1 Tests

---
+++ day1/run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def calc_fuel(mass):
+    return max(int(mass / 3) - 2, 0)
+
+def test_calc_fuel_example1():
+    assert calc_fuel(12) == 2
+
+def test_calc_fuel_example2():
+    assert calc_fuel(14) == 2
+
+def test_calc_fuel_example3():
+    assert calc_fuel(1969) == 654
+
+def test_calc_fuel_example4():
+    assert calc_fuel(100756) == 33583
+
+if __name__ == "__main__":
+    print("Day 1")
