e0491dfd59eeb37ed7424c0fbe8609a33437c232
Manuel Rothenberg <m.rothenberg@samsung.com>
2019-12-03 13:00:58 +0100

    Rename tests to match functions

--- day02/run_p2.py
+++ day02/run_p2.py
@@ -6,16 +6,16 @@ import sys
 import math
 
 
-def test_exectue_program_step_example1():
+def test_exectue_program_example1():
     assert execute_program([1,0,0,0,99]) == [2,0,0,0,99]
 
-def test_exectue_program_step_example2():
+def test_exectue_program_example2():
     assert execute_program([2,3,0,3,99]) == [2,3,0,6,99]
 
-def test_exectue_program_step_example3():
+def test_exectue_program_example3():
     assert execute_program([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
 
-def test_exectue_program_step_example4():
+def test_exectue_program_example4():
     assert execute_program([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
 
 
